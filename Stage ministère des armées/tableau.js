a55b8d33a3dc630394294a2e9488e7dd6c24de75
Nyssa0 <72558001+Nyssa0@users.noreply.github.com>
2021-05-18 19:55:48 +0200

    barre de recherche

--- Stage ministère des armées/tableau.js
+++ Stage ministère des armées/tableau.js
@@ -333,3 +333,12 @@ document.querySelectorAll(".table-sortable th").forEach(headerCell => {
         sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
     });
 });
+
+    $(document).ready(function(){
+    $("#searchBox").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#table tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
